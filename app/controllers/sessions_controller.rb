77e8f8425a4c1ee54fdef8e8f734bc26cb4ed5f4
Charles Smith <charles@charlesesmith.net>
2016-05-28 17:17:00 -0700

    Added login/out functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    render 'new'
+  end
+  
+  def create
+    user = User.find_by(emp_no: params[:session][:emp_no].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid employee number/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  root 'users#index'
-  get 'signup' => 'users#new'
+  root   'users#index'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 end
