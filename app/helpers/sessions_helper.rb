2d03b0d8883a3f83fa857dfb58bbf4df4f6ec6b8
Charles Smith <charles@charlesesmith.net>
2016-05-28 18:06:33 -0700

    added a little more clean up to users.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,15 @@
 module SessionsHelper
- def log_in(user)
+  def log_in(user)
     session[:user_id] = user.id
- end
+  end
 
- def current_user
-   @current_user ||= User.find_by(id: session[:user_id])
- end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
 
   def logged_in?
     !current_user.nil?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528001540) do
+ActiveRecord::Schema.define(version: 20160529003319) do
 
   create_table "users", force: :cascade do |t|
     t.integer  "emp_no"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160528001540) do
     t.string   "fname"
     t.string   "lname"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
