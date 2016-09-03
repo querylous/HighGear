6c98b823a051cfddae977a423e0378c541e0b777
Charles Smith <charles@charlesesmith.net>
2016-09-03 16:31:32 -0700

    changed route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  root   'users#index'
+  root   'foods#index'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
