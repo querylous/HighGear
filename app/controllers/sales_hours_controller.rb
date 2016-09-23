07616c26ba59e88f25650a8253b7d45681936bd4
Charles Smith <charles@charlesesmith.net>
2016-09-23 15:40:56 -0700

    added migration to change projected/actual vlh to decimal.

--- app/controllers/sales_hours_controller.rb
+++ app/controllers/sales_hours_controller.rb
@@ -2,8 +2,10 @@ class SalesHoursController < ApplicationController
   def index
     if params[:date].nil?
       @start_date = Time.now.beginning_of_day + 4.hours
+      logger.info "date is #{@start_date}"
     else
       @start_date = (Time.parse(params[:date].to_s).beginning_of_day) + 4.hours
+      logger.info "date is #{@start_date}"
     end
     end_date = @start_date.end_of_day + 3.hours
     @sales_hours = SalesHour.where(
@@ -18,8 +20,17 @@ class SalesHoursController < ApplicationController
   end
   
   def update
-
-
+    @sales_hour = SalesHour.find(params[:id]) 
+    @sales_hour.update_attributes!(sales_hour_params)
+    @sales_hour.update_attributes!(user_id: params[:user_id])
+    respond_to do |format|
+    format.html {
+    if request.xhr?
+      render :text => params[:sales_hour].values.first
+    else
+    end
+    }
+    end
   end
   
   def destroy
@@ -27,4 +38,13 @@ class SalesHoursController < ApplicationController
 
   end
   
+  
+  private 
+  
+  def sales_hour_params
+    params.require(:sales_hour).permit(:datetime, :projected_sales, :actual_sales,
+                                       :projected_vlh, :actual_vlh, :ttl, 
+                                       :store_number, :user_id, :id)
+end
+ 
 end
--- app/helpers/sales_hour_helper.rb
+++ app/helpers/sales_hour_helper.rb
@@ -13,19 +13,97 @@ module SalesHourHelper
       p[:date].gsub!("'", "")
       p[:hour].gsub!("'", "")
       if p[:hour] == "25:00"
-        logger.info "#{p[:date]} " + "#{p[:hour]}"
         p[:hour].gsub!("25:00", "01:00")
-        logger.info "#{p[:date]} " + "#{p[:hour]}" 
-        time = "#{p[:date]} " + "#{p[:hour]}"
-        time = DateTime.strptime("#{time}", '%m/%d/%Y %H:%M').to_time
-        logger.info time
+        time = "#{p[:date]} " + "#{p[:hour]} -07:00"
+        time = DateTime.strptime("#{time}", '%m/%d/%Y %H:%M')
         time = time + 1.day
-        logger.info time.localtime
       else
-        time = "#{p[:date]} " + "#{p[:hour]}"
-        time = DateTime.strptime("#{time}", '%m/%d/%Y %H:%M').to_time
+        time = "#{p[:date]} " + "#{p[:hour]} -07:00"
+        time = DateTime.strptime("#{time}", '%m/%d/%Y %H:%M %Z')
+        logger.info time.zone
       end
       @formatted_projections.push( { :time => time, :sales => p[:sales] })
     end 
+
+    @formatted_projections.last(144).each do |p|
+      unless SalesHour.exists?(:datetime => p[:time])
+        hour = SalesHour.new
+        hour.datetime = p[:time]
+        hour.projected_sales = p[:sales]
+        hour.store_number = 4453
+        hour.save
+      end
+    end
+  end
+ 
+  def process_schedule
+    file = './schedule.csv'
+    options = { :remove_unmapped_keys => true, 
+                :key_mapping => 
+              { :date => :date,
+                :hour_end_time => :hour,
+                :projected_vlh => :vlh
+    } }
+    @formatted_schedule = [] 
+    projections = SmarterCSV.process(file, options)
+    projections.each do |p|
+      p[:date].gsub!("'", "")
+      p[:hour].gsub!("'", "")
+      if p[:hour] == "25:00"
+        p[:hour].gsub!("25:00", "01:00")
+        time = "#{p[:date]} " + "#{p[:hour]} -07:00"
+        time = DateTime.strptime(time, '%m/%d/%Y %H:%M %Z')
+        time = time + 1.day
+      elsif p[:hour] == "26:00"
+        p[:hour].gsub!("26:00", "02:00")
+        time = "#{p[:date]} " + "#{p[:hour]} -07:00"
+        time = DateTime.strptime(time, '%m/%d/%Y %H:%M %Z')
+        time = time + 1.day
+       elsif p[:hour] == "27:00"
+        p[:hour].gsub!("27:00", "03:00")
+        time = "#{p[:date]} " + "#{p[:hour]} -07:00"
+        time = DateTime.strptime(time, '%m/%d/%Y %H:%M %Z')
+        time = time + 1.day
+      else
+        time = "#{p[:date]} " + "#{p[:hour]} -07:00"
+        logger.info time
+        time = DateTime.strptime("#{time}", '%m/%d/%Y %H:%M %Z')
+      end
+      @formatted_schedule.push( { :time => time, :projected_vlh => p[:vlh] })
+    end 
+
+    @formatted_schedule.last(144).each do |p|
+      hour = SalesHour.find_by(datetime: p[:time])
+      unless hour.nil? 
+        hour.projected_vlh = p[:projected_vlh]
+        hour.save
+      end
+    end   
+  end
+ 
+  def get_total_sales_hours(id, time, col)
+    selected_hours = SalesHour.where(datetime: time) 
+    selected_hours = selected_hours.where(user_id: id) unless id.nil? 
+    logger.info selected_hours.sum(col)
+    return selected_hours.sum(col)
+  end
+  
+  def get_sales_up_down_today
+    selected_hours = SalesHour.where(datetime: (Time.now.beginning_of_day + 3.hours)..Time.now)
+    total = 0
+    selected_hours.each do |s|
+        total += s.actual_sales - s.projected_sales unless s.actual_sales.nil?
+    end
+    return total
+  end
+  
+  def get_labor_up_down_today
+    selected_hours = SalesHour.where(datetime: (Time.now.beginning_of_day + 3.hours)..Time.now)
+    total = 0
+    selected_hours.each do |s|
+        total += s.actual_vlh - s.projected_vlh unless s.actual_vlh.nil?
+    end
+    return total
+
   end
 end
