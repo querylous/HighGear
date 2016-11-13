7dddc27374a25f995e3e75a67a16eb97b1be2393
Charles Smith <charles@charlesesmith.net>
2016-11-13 13:23:45 -0800

    added manager in charge.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,18 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :login_first
 
+  $current_manager = nil
 
   def login_first
     unless logged_in?
       flash[:error] = "You must be logged in to access this application."
       redirect_to login_url
     end
+  end
+  
+  def current_manager
+    $current_manager = current_user
+    flash[:success] = "You're in charge #{current_user.fname}"
+    redirect_to root_path
   end
 end
